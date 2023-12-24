47fbc732fcfe198c730510f9f1faf03981e43889
@Faimasoft786 <FatimaSoft786@gmail.com>
2023-12-24 20:19:11 +0500

    Body edit

--- src/Home/home.js
+++ src/Home/home.js
@@ -15,7 +15,7 @@ const Home = () => {
 
 
   return (
-    <div className="max-w-full flex flex-col mt-14 ">
+    <div className="w-full h-full flex flex-col mt-20 ">
     {/* banner */}
       <div className="relative flex flex-col w-full">
         <img
@@ -23,7 +23,7 @@ const Home = () => {
           alt="BannerImage"
           className="absolute h-[500px] w-full object-cover "
         />
-        <div className=" flex lg:flex-row sm:flex-col xs:flex-col lg:justify-between absolute w-full h-10 lg:mt-10 lg:space-y-0 sm:mt-10 xs:mt-10 xs:space-y-4 xs:w-full sm:space-y-4 ">
+        <div className=" flex lg:flex-row sm:flex-col xs:flex-col lg:justify-between absolute w-full h-[50px] lg:mt-14 lg:space-y-0 sm:mt-10 xs:mt-10 xs:space-y-4 xs:w-full sm:space-y-4 ">
           <div className=" lg:mx-16 xs:mx-6 bg-white rounded-[2px] flex flex-row lg:w-1/3 sm:w-auto justify-end">
             <div className="w-auto h-full bg-black">
               <input
@@ -34,7 +34,7 @@ const Home = () => {
 
             <div className=" w-2/3 lg:flex-1 xs:flex lg:justify-end xs:justify-center h-full ">
               <select
-                className=" lg:pb-2.5 appearance-none h-full lg:text-end lg:items-end xs:text-center xs:items-center font-roboto font-normal lg:text-sm xs:text-xs w-full border-none outline-none"
+                className=" lg:pb-3.5 appearance-none h-full lg:text-end lg:items-end xs:text-center xs:items-center font-roboto font-normal lg:text-sm xs:text-xs w-full border-none outline-none"
                 id="dropdown"
                 value={selectedOption}
                 onChange={handleSelectChange}
@@ -48,27 +48,30 @@ const Home = () => {
               </select>
             </div>
 
-            <div className=" w-auto h-full flex justify-center items-center mx-2">
-              <IoIosArrowDown size={24} />
+            <div className=" w-auto h-full flex justify-center items-center mx-2 my-0.5">
+              <IoIosArrowDown size={22} />
             </div>
 
-            <div className=" bg-primary w-11 h-full flex justify-center">
-              <img className="item-center lg:p-2 xs:p-1" src={search} alt="test" />
+            <div className=" bg-primary w-[50px] h-[50px] flex justify-center">
+              <img className="item-center lg:p-3 xs:p-1" src={search} alt="test" />
             </div>
           </div>
-          <p className=" lg:mx-16 xs:mx-6 px-5 h-10 pt-2 bg-primary rounded-[2px] text-white font-roboto font-normal text-base">
+          <div className="lg:mx-16 xs:mx-6 h-[50px] bg-primary rounded-[2px] text-white font-roboto font-semibold text-sm flex justify-center">
+          <p className=" text-center items-center px-7 pt-4 h-full">
             Place an AD
           </p>
+          </div>
+          
         </div>
    
     <div className=" relative flex flex-col lg:w-full sm:w-full xs:w-full lg:mt-32 xs:mt-40 sm:mt-40 justify-start items-start h-auto lg:mx-10 xs:px-6">
-    <p className="text-white font-roboto text-start font-semibold lg:w-[500px] xs:w-full sm:w-full lg:text-4xl xs:text-2xl">Diversified And Unvarying Material</p>
-    <p className=" text-white font-normal font-roboto text-start mt-5 lg:text-base xs:text-sm xs:w-[300px] lg:w-[500px]">Lorem ipsum dolor sit amet consetetur Lorem ipsum dolor sit amet consetetur Lorem ipsum dolor sit amet consetetur Lorem ipsum dolor sit amet consetetur Lorem ipsum dolor sit amet consetetur.</p>
+    <p className="text-white font-roboto text-start  font-medium lg:mt-12 lg:w-[600px] xs:w-full sm:w-full lg:text-5xl xs:text-2xl">Diversified And Unvarying Material</p>
+    <p className=" text-white font-light font-roboto text-start mt-5 lg:text-base xs:text-sm xs:w-[300px] lg:w-[500px]">Lorem ipsum dolor sit amet consetetur Lorem ipsum dolor sit amet consetetur Lorem ipsum dolor sit amet consetetur Lorem ipsum dolor sit amet consetetur Lorem ipsum dolor sit amet consetetur.</p>
     </div>
 
       </div>
    
-   <div className=" lg:mt-[220px] xs:mt-[180px] flex justify-center">
+   <div className=" lg:mt-[180px] xs:mt-[180px] flex justify-center">
     <h1 className=" font-roboto font-semibold text-xl lg:h-10 items-center">New Advertisement</h1>
    </div>
 
